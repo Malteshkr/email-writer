8e2c058db5e2f59ec5792b120779f301bbf939f6
Malteshkr <67941745+Malteshkr@users.noreply.github.com>
2025-03-15 14:57:12 +0530

    enabling ai changes

---
+++ src/main/java/com/email/writer/controller/EmailGeneratorController.java
@@ -0,0 +1,24 @@
+package com.email.writer.controller;
+
+import com.email.writer.repo.EmailRequest;
+import com.email.writer.service.EmailGeneratorService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/emil")
+@AllArgsConstructor
+@CrossOrigin(origins="*")
+public class EmailGeneratorController {
+
+    @Autowired
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
